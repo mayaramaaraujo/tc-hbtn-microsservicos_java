e545294f70a3713473cacf8eba9bf61dfa95292f
Mayara Araujo <63872581+mayaramaaraujo@users.noreply.github.com>
2022-04-04 08:07:22 -0300

    Add files via upload

--- 0x02/construindo_API_REST/com/song/demo/Song.java
+++ 0x02/construindo_API_REST/com/song/demo/Song.java
@@ -1,5 +1,80 @@
 package com.example.demo;
 
-public class Song {
+import java.util.Objects;
 
+public class Song {
+	private Integer id;
+	private String nome;
+	private String artista;
+	private String album;
+	private String anoLancamento;	
+	
+	public Song(Integer id, String nome, String artista, String album, String anoLancamento) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.artista = artista;
+		this.album = album;
+		this.anoLancamento = anoLancamento;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getArtista() {
+		return artista;
+	}
+	public void setArtista(String artista) {
+		this.artista = artista;
+	}
+	public String getAlbum() {
+		return album;
+	}
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+	public String getAnoLancamento() {
+		return anoLancamento;
+	}
+	public void setAnoLancamento(String anoLancamento) {
+		this.anoLancamento = anoLancamento;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(album, anoLancamento, artista, id, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Song other = (Song) obj;
+		return Objects.equals(album, other.album) && Objects.equals(anoLancamento, other.anoLancamento)
+				&& Objects.equals(artista, other.artista) && Objects.equals(id, other.id)
+				&& Objects.equals(nome, other.nome);
+	}
+
+	@Override
+	public String toString() {
+		return "Song [id=" + id + ", nome=" + nome + ", artista=" + artista + ", album=" + album + ", anoLancamento="
+				+ anoLancamento + "]";
+	}
+	
+	
+	
+	
 }
--- 0x02/construindo_API_REST/com/song/demo/SongRepository.java
+++ 0x02/construindo_API_REST/com/song/demo/SongRepository.java
@@ -1,5 +1,31 @@
 package com.example.demo;
 
-public class SongRepository {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SongRepository {
+	private List<Song> list = new ArrayList<Song>();
+
+    public List<Song> getAllSongs() {
+     return list;
+    }
+
+    public Song getSongById(Integer id) {
+       // TODO
+    }
+
+    public void addSong(Song s) {
+      // TODO
+    }
+
+    public void updateSong(Song s) {
+      // TODO
+    }
+
+    public void removeSong(Song s) {
+       // TODO
+    }
 }
